651adbfb07d63eab9b9b3b4144d1888c49e58a17
null <saturngdformerlysye@gmail.com>
2024-08-17 06:04:59 -0400

    actually uploaded the files lol

---
+++ example_mods_no_github/scripts/wo-ah-mark.lua
@@ -0,0 +1,5 @@
+function onCreatePost()
+    makeLuaText('watermark', songName .. " - Nullspace Engine (PE 0.7.3)", 0, 2, 695);
+    setTextSize('watermark', 18);
+    addLuaText('watermark');
+end
---
+++ example_mods_no_github/stages/CHEATING.lua
@@ -0,0 +1,6 @@
+function onCreatePost()
+    makeLuaSprite('cheating', 'cheating', -500, -150);
+    addLuaSprite('cheating')
+    --scaleObject('joe-moma', 8, 8)
+    addGlitchEffect('cheating', 2, 5)
+end
